2f5f36f3daa45c8392312f9a45c2e4541e572c05
arya-2407 <aryapofficial@gmail.com>
2024-09-16 13:26:46 -0700

    typedef struct

--- structures/basics.c
+++ structures/basics.c
@@ -7,6 +7,12 @@ struct Player{
     int age;
 };
 
+typedef struct{
+    char username[25];
+    char password[10];
+    int id;    
+} User;
+
 
 
 int main(){
@@ -23,6 +29,11 @@ int main(){
 
     printf("Player 1 Details: \nName: %s\nAge: %d\n",player1.name,player1.age);
     printf("Player 2 Details: \nName: %s\nAge: %d\n",player2.name,player2.age);
+
+    //using typedef struct
+
+    User user1 = {"User1","123",1};
+    printf("User 1 Details: \nusername: %s\npassword: %s\nid: %d\n",user1.username,user1.password,user1.id);
 
 
     return 0;
